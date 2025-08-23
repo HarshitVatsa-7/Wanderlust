a7928a2082ea3a99e03d42e055d5437607fad543
HarshitVatsa-7 <144253711+HarshitVatsa-7@users.noreply.github.com>
2025-08-23 15:49:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -70,12 +70,13 @@ const sessionOptions = {
     store,
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: process.env.NODE_ENV === "production"
+        sameSite: "lax"
     },
 };
 
